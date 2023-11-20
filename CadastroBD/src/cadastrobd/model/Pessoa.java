f9edaf4e2e156e4c290def9e4f439461edef75b0
Anna Eliza <annaelizadev@gmail.com>
2023-11-20 17:41:26 -0300

    Add files via upload

---
+++ CadastroBD/src/cadastrobd/model/Pessoa.java
@@ -0,0 +1,32 @@
+package cadastrobd.model;
+
+public record Pessoa(int idPessoa, String nome, String logradouro, String cidade, String estado, String telefone, String email) {
+
+    // Construtor padrão
+    public Pessoa {
+        validarCampos();
+    }
+
+    private void validarCampos() {
+        if (idPessoa < 0) {
+            throw new IllegalArgumentException("ID não pode ser negativo");
+        }
+        // Adicione outras validações conforme necessário para os outros campos.
+    }
+
+    // Método para exibir os dados no console
+    public void exibir() {
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Id: " + idPessoa +
+                "\nNome: " + nome +
+                "\nLogradouro: " + logradouro +
+                "\nCidade: " + cidade +
+                "\nEstado: " + estado +
+                "\nTelefone: " + telefone +
+                "\nE-mail: " + email;
+    }
+}
